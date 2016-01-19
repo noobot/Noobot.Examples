d0f21a205670e788312752850ea8f41533ce25f8
Simon C <simon_colmer@hotmail.com>
2016-01-19 19:46:34 +0000

    Added ConsoleService base app

---
+++ src/Noobot.Examples.ConsoleService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noobot.Examples.ConsoleService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
